de5e0919babe987484986c2b39e3cded325340a3
Donnie Adams <donnie@thedadams.com>
2026-01-01 13:58:38 -0500

    feat: add support for prefix styling (#61)
    This change allows the prefix of the comment `//` or `#` to be styled
    independently from the rest of the comment.
    
    Signed-off-by: Donnie Adams <donnie@thedadams.com>

--- languages/comment/highlights.scm
+++ languages/comment/highlights.scm
@@ -1,31 +1,47 @@
 ((tag
+  (prefix)? @constant.comment.todo.prefix
   (name) @_name @constant.comment.todo
   ("(" @constant.comment.todo.bracket
     (user) @constant.comment.todo.user
     ")" @constant.comment.todo.bracket)?
-  (text)? @constant.comment.todo.text)
+  (
+    (prefix)? @constant.comment.todo.prefix
+    (text)? @constant.comment.todo.text)
+  )
   (#match? @_name "^[</#*;+\\-!| \t]*(TODO|WIP|MAYBE)$"))
 
 ((tag
+  (prefix)? @string.comment.info.prefix
   (name) @_name @string.comment.info
   ("(" @string.comment.info.bracket
     (user) @string.comment.info.user
     ")" @string.comment.info.bracket)?
-  (text)? @string.comment.info.text)
+  (
+    (prefix)? @string.comment.info.prefix
+    (text)? @string.comment.info.text)
+  )
 (#match? @_name "^[</#*;+\\-!| \t]*(NOTE|XXX|INFO|DOCS|PERF|TEST)$"))
 
 ((tag
+  (prefix)? @property.comment.error.prefix
   (name) @_name @property.comment.error
   ("(" @property.comment.error.bracket
     (user) @property.comment.error.user
     ")" @property.comment.error.bracket)?
-  (text)? @property.comment.error.text)
+  (
+    (prefix)? @property.comment.error.prefix
+    (text)? @property.comment.error.text)
+  )
 (#match? @_name "^[</#*;+\\-!| \t]*(FIXME|BUG|ERROR|DELETE)$"))
 
 ((tag
+  (prefix)? @keyword.comment.warn.prefix
   (name) @_name @keyword.comment.warn
   ("(" @keyword.comment.warn.bracket
     (user) @keyword.comment.warn.user
     ")" @keyword.comment.warn.bracket)?
-  (text)? @keyword.comment.warn.text)
+  (
+    (prefix)? @keyword.comment.warn.prefix
+    (text)? @keyword.comment.warn.text)
+  )
 (#match? @_name "^[</#*;+\\-!| \t]*(HACK|WARNING|WARN|FIX|SAFETY)$"))
