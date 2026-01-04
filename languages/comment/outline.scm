7318663e9266e72b777001bd4f28590f291f5206
Donnie Adams <donnie@thedadams.com>
2026-01-04 14:19:13 -0500

    feat: add *, !, # and ? as special tag names (#65)
    Signed-off-by: Donnie Adams <donnie@thedadams.com>

--- languages/comment/outline.scm
+++ languages/comment/outline.scm
@@ -6,5 +6,5 @@
       ")" @context
   )?
   (text)? @context
-  (#match? @name "^(TODO|WIP|MAYBE|NOTE|XXX|INFO|DOCS|PERF|TEST|FIXME|BUG|ERROR|DELETE|HACK|WARNING|WARN|FIX|SAFETY)$")
+  (#match? @name "^(TODO|WIP|MAYBE|NOTE|XXX|INFO|DOCS|PERF|TEST|FIXME|BUG|ERROR|DELETE|HACK|WARNING|WARN|FIX|SAFETY|QUESTION|\\*|!|\\?|#)$")
   ) @item
