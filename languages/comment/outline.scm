c95735f970ac98c51b688186b458a5522fe13005
Donnie Adams <donnie@thedadams.com>
2026-01-20 20:00:37 -0500

    fix: only the first text should be used as context (#73)
    Signed-off-by: Donnie Adams <donnie@thedadams.com>

--- languages/comment/outline.scm
+++ languages/comment/outline.scm
@@ -5,6 +5,7 @@
       (user) @context
       ")" @context
   )?
+  .
   (text)? @context
   (#match? @name "^(TODO|WIP|MAYBE|NOTE|XXX|INFO|DOCS|PERF|TEST|FIXME|BUG|ERROR|DELETE|HACK|WARNING|WARN|FIX|SAFETY|QUESTION|\\*|!|\\?|#)$")
   ) @item
